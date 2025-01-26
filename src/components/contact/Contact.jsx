d6414baa3987b814e986edd3b0e7294a03b139e5
lyklecharova <lklecharova9@gmail.com>
2025-01-26 17:21:48 +0200

    Added a contact form with email submission functionality using @emailjs/browser - Created a user-friendly form with fields for name, email, and message - Integrated emailjs to send form data directly to email upon submission - Displayed success and error messages based on the outcome of the form submission

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,74 @@
-import "./contact.css"
+import { useRef, useState } from 'react';
+import './contact.css';
+import emailjs from '@emailjs/browser';
 const Contact = () => {
-    return (
-        <div className='contact'>Contact</div>
-    )
-}
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
 
-export default Contact
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                import.meta.env.VITE_SERVICE_ID,
+                import.meta.env.VITE_TEMPLATE_ID,
+                form.current,
+                {
+                    publicKey: import.meta.env.VITE_PUBLIC_KEY,
+                }
+            )
+            .then(
+                () => {
+                    setSuccess(true);
+                    setError(false);
+                },
+                (error) => {
+                    setError(true);
+                    setSuccess(false);
+                }
+            );
+    };
+    return (
+        <div className="contact">
+            <div className="cSection">
+                <form ref={form} onSubmit={sendEmail}>
+                    <h1 className="cTitle">Let's keep in touch</h1>
+                    <div className="formItem">
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            name="user_username"
+                            placeholder="John Doe"
+                        />
+                    </div>
+                    <div className="formItem">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="user_email"
+                            placeholder="john@gmail.com"
+                        />
+                    </div>
+                    <div className="formItem">
+                        <label>Message</label>
+                        <textarea
+                            rows={10}
+                            name="user_message"
+                            placeholder="Write your message..."
+                        ></textarea>
+                    </div>
+                    <button type="submit" className="formButton">
+                        Send
+                    </button>
+                    {success && <span>Your message has been sent!</span>}
+                    {error && <span>Something went wrong!</span>}
+                </form>
+            </div>
+            <div className="cSection">SVG</div>
+        </div>
+    );
+};
+
+export default Contact;
